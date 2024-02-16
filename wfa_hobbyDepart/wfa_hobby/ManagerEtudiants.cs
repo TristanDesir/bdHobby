2e824f996f270d55ecd9d599b7146e69f2db7329
Philippe-Morin-Veer <157501225+Philippe-Morin-Veer@users.noreply.github.com>
2024-02-16 09:44:32 -0500

    fin cour 1

---
+++ wfa_hobbyDepart/wfa_hobby/ManagerEtudiants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfa_hobby
+{
+    internal class ManagerEtudiants
+    {
+        public int AjouterEtudiants(Etudiants etudiants)
+        {
+         
+            int nombreDeLigneAffectee = 0;
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}

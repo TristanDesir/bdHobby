b97ec8729a92654d490aa20f4d652a47017a1e82
TristanDesir <157501494+TristanDesir@users.noreply.github.com>
2024-02-23 08:55:26 -0500

    Lister provenance

---
+++ wfa_hobbyDepart/wfa_hobby/ManagerProvenance.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfa_hobby
+{
+    internal class ManagerProvenance
+    {
+        public List<Provenance> ListerProvenance()
+        {
+            List<Provenance>
+            using(var maConnexion = new SqlConnection(Properties.Settings.Default.maConnectionString))
+            {
+                using (var maCommande = new SqlCommand("Lister Provenance", maConnexion))
+                {
+                    // definir commandType
+                    maCommande.CommandType = System.Data.CommandType.StoredProcedure;
+                    // definir parametres 
+                    
+                    // ouvrir connexion
+                    // executer sp
+                    using (var monDataReader = maCommande.ExecuteReader())
+                    {
+                        while (monDataReader.Read())
+                        {
+                            Provenance provenance = new Provenance();
+                            provenance.No_provenance = (int)monDataReader["no_provenance"]
+                            provenance.ProvenanceDescription = monDataReader["Description"].ToString();
+                            maListe.Add(provenance);
+                        }
+                    }
+                    // lire resultats
+                }
+
+            }
+            return maListe;
+        }
+    }
+}

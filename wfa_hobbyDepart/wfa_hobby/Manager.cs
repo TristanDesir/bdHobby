3642ad7d3623b7fdc6b9b26238ba880d1f6fca4c
TristanDesir <157501494+TristanDesir@users.noreply.github.com>
2024-02-26 17:02:23 -0500

    commit

--- wfa_hobbyDepart/wfa_hobby/Manager.cs
+++ wfa_hobbyDepart/wfa_hobby/Manager.cs
@@ -10,6 +10,14 @@ namespace wfa_hobby
     internal class Manager
     {
         // protected signifie utilisable que par ses filles, sans new
+
+        protected SqlCommand CreerCommande(string nomProcedureStockee,List<SqlParameter> mesParametresSql)
+        {
+            var maCommand = new SqlCommand(nomProcedureStockee,GetConnection());
+            // definir le commandtype
+        }
+
+        protected SqlConnection
         
     }
 }
